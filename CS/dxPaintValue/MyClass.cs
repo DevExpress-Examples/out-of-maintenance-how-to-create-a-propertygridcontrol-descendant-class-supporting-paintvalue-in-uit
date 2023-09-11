0033a88afa5f2b1cd789e5ffa3a34b618494e877
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-11 16:31:10 +0400

    Change example status to out of maintenance

--- CS/dxPaintValue/MyClass.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel;
-using System.Drawing.Design;
-
-namespace dxPaintValue {
-    public class MyClass {
-        [Editor(typeof(MyPropertyEditor), typeof(UITypeEditor))]
-        public string Color { get; set; }
-    }
-}
--- VB/dxPaintValue/MyClass.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.ComponentModel
-Imports System.Drawing.Design
-
-Namespace dxPaintValue
-    Public Class [MyClass]
-        <Editor(GetType(MyPropertyEditor), GetType(UITypeEditor))> _
-        Public Property Color() As String
-    End Class
-End Namespace
